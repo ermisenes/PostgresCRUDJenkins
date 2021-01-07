1f041d1d594c5df07aa971523e0f54b4fe954667
ENES ERMİŞ <enes-ermis@hotmail.com>
2021-01-07 18:41:20 +0300

    WeatherForecastController'a  Türkçe hava durumları eklendi

--- PostgresCRUD/Controllers/WeatherForecastController.cs
+++ PostgresCRUD/Controllers/WeatherForecastController.cs
@@ -14,6 +14,7 @@ namespace PostgresCRUD.Controllers
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            //"Donma", "Destekleme", "Soğuk", "Soğuk", "Hafif", "Sıcak", "Balsam", "Sıcak", "Sıcak", "Kavurucu"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
